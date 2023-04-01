cdde2ffa1b2a7cd7e55831f9691cac34b6aa2028
Lior Vinman <liorvi35@gmail.com>
2023-04-01 17:32:36 +0300

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,64 +1,84 @@
 """
 this file contains the implementation for physical address, i.e. MAC (Media Access Control).
 :author: Lior Vinman
-:version: 1.0
+:version: 1.1
 :date: 01/04/2023
 """
 
 import subprocess
 import optparse
+import random
 import re
 
 
-def get_args():
+def get_random_mac():
     """
-    this function is getting the interface name MAC address from standard input
-    :return: settings from input, if successes
+    this function generates random MAC address
+    :return: new generated MAC
+    """
+    octets = [random.randint(0x00, 0xff) for i in range(6)]
+    octets[0] &= 0xfe
+    random_mac = ":".join("{:02x}".format(octet) for octet in octets)
+    return random_mac
+
+
+def get_arguments():
+    """
+    this function gets arguments from standard input (-i <interface> -m <mac>)
+    :return: options with interface and mac
     """
     parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface")
-    parser.add_option("-m", "--mac", dest="mac", help="Mac")
+    parser.add_option("-i", "--interface", dest="inter",
+                      help="Network interface that it's MAC-address should be changed")
+    parser.add_option("-m", "--mac", dest="mac", help="new MAC-address to change to")
     (options, arguments) = parser.parse_args()
-
-    if not options.interface:
+    if not options.inter:
         parser.error("[-] Please specify an interface, use --help for more info")
     elif not options.mac:
-        parser.error("[-] Please specify a MAC address, use --help for more info")
+        parser.error("[-] Please specify a MAC-address, use --help for more info")
+    else:
+        return options
 
-    return options
 
-
-def change_mac(interface, new_mac):
+def change_mac(interface, mac):
     """
-    this function is changing the MAC address on the given interface
-    :param interface: the device to change to the MAC address
-    :param new_mac: the new address to set
+    this function changes the MAC address on given interface
+    :param interface: network device to change MAC on
+    :param mac: MAC to change to
     """
+    new_mac = mac
+    if mac == "random":
+        new_mac = get_random_mac()
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 
-def current_mac(interface):
+def get_current_mac(interface):
     """
     this function returns the current MAC address on given interface
     :param interface: device to check current MAC
-    :return: current address, if successes
+    :return: current address, if successes, else None
     """
-    ifconfig_out = subprocess.call(["ifconfig", interface])
-    res = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_out))
-
-    if res:
-        return res
+    ifconfig_out = subprocess.check_output(["ifconfig", interface]).decode("utf-8")
+    mac_regex = re.compile(r"ether ([\da-fA-F]{2}(?::[\da-fA-F]{2}){5})")
+    match = mac_regex.search(ifconfig_out)
+    if match:
+        mac_address = match.group(1)
+        return mac_address
     else:
-        print("[-] Cannot find MAC address")
+        return None
 
 
 def main():
-    options = get_args()
-    m = current_mac(options.interface)
-    change_mac(options.interface, options.mac)
-    m = current_mac(options.interface)
+    opts = get_arguments()
+    old_mac = get_current_mac(opts.inter)
+    change_mac(opts.inter, opts.mac)
+    new_mac = get_current_mac(opts.inter)
+    if old_mac != new_mac:
+        print(f"[+] {opts.inter} has changed MAC from {old_mac} to {new_mac}")
+    else:
+        print(f"[-] cannot change MAC on {opts.inter}")
 
 
 if __name__ == "__main__":
